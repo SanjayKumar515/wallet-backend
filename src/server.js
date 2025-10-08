a72a204c0375f5c0773b21c8cef33a7be923fd7e
Sanjay Kumar <sanjubabu9419@gmail.com>
2025-10-08 15:25:08 +0530

    Update server.js

--- src/server.js
+++ src/server.js
@@ -4,9 +4,14 @@ import { initDB } from "./config/db.js";
 import rateLimiter from "./middleware/rateLimiter.js";
 import transactionsRoute from "./routes/transactionsRoute.js";
 
+ import job from "./config/cron.js";
+
+
 dotenv.config();
 
 const app = express();
+
+if (process.env.NODE_ENV==="production") job.start()
 
 //middleware
 app.use(rateLimiter)
