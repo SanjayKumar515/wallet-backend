26073c52cac138342de1caa7af53e6e08c0087d0
Sanjay Kumar <sanjubabu9419@gmail.com>
2025-10-08 17:38:35 +0530

    new

--- src/middleware/rateLimiter.js
+++ src/middleware/rateLimiter.js
@@ -1,22 +1,20 @@
 import ratelimit from "../config/upstash.js";
 
-const rateLimiter = async(req,res,next)=>{
-    try {
-        // here we kept it simple  in real world we can use user id or ip address to identify user
-        // and set different rate limit for different users
-        // like free user can have 100 requests per hour and paid user can have 1000 requests per hour
-        // also we can have different rate limit for different routes
-        // like login route can have 10 requests per hour and other routes can have 100 requests per hour
-        const {success} = await ratelimit.limit("My global rate limit");
-        if(!success) return res.status(429).json({message:"Too many requests , please try again later"})
-        next();
-    } catch (error) {
-        console.log("Error in rate limiter middleware ",error);
-        res.status(500).json({message:"Internal server error!"});
+const rateLimiter = async (req, res, next) => {
+  try {
+    const { success } = await ratelimit.limit("my-rate-limit");
 
+    if (!success) {
+      return res.status(429).json({
+        message: "Too many requests, please try again later.",
+      });
     }
 
-} 
-
+    next();
+  } catch (error) {
+    console.log("Rate limit error", error);
+    next(error);
+  }
+};
 
 export default rateLimiter;
